2b9f4fef51dcb31fd6a46d3201201112c69f7a2e
hanuman <a.kolyan@gratun.am>
2021-11-26 15:18:54 +0400

    initial commit

---
+++ onerune/sketch.js
@@ -0,0 +1,18 @@
+let castrune
+
+async function preload() {
+    const rndNum = await getQuantumRandomNumbers()
+    const rune = futhark[int(map(rndNum[0], 0, 255, 0, futhark.length-1))]
+    castrune = genDrawLines(rune, 350, 250, 0, 2)
+}
+
+function setup() {
+    createCanvas(800, 800)
+    background("black")
+}
+
+function draw() {
+    if(castrune){
+        castrune.next()
+    }
+}
