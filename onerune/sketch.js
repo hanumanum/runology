09f40f50f4f88e86025e9d829b964208f9f4c632
hanuman <a.kolyan@gratun.am>
2021-12-31 20:40:55 +0400

    one rune caster completed

--- onerune/sketch.js
+++ onerune/sketch.js
@@ -1,22 +1,65 @@
 //DEBUG GLOBALS
-const SHOWBOUNDS = true
-const SHOWCENTER = true
+const SHOWBOUNDS = false
+const SHOWCENTER = false
+
+const centerX =window.innerWidth/2
+const centerY =window.innerHeight/2
 
 let castrune
+let startCasting
 
 async function preload() {
-    const rndNum = await getQuantumRandomNumbers()
-    const rune = futhark[int(map(rndNum[0], 0, 255, 0, futhark.length-1))]
-    castrune = genDrawLines(rune, 350, 250, 0, 2)
+    
 }
 
 function setup() {
-    createCanvas(800, 800)
+    createCanvas(window.innerWidth, window.innerHeight)
     background("black")
+    angleMode(DEGREES)
+    
 }
 
 function draw() {
+    drawRuneCirlce(futhark, centerX, centerY, 400)
+    
+   
     if(castrune){
         castrune.next()
+    }
+    else{
+        fill("white")
+        textSize(25)
+        textAlign(CENTER, CENTER);
+        text("Ask your question", centerX, centerY)
+        text("Click here", centerX, centerY+50)
+        noLoop()
+    }
+    
+    
+}
+
+async function mouseClicked(){
+    if(castrune) return;
+    const rndNum = await getQuantumRandomNumbers()
+    const rune = futhark[int(map(rndNum[0], 0, 255, 0, futhark.length-1))]
+    castrune = genDrawLines(rune, centerX-50, centerY-140, 0, 2)
+    background("black")
+    loop()
+}
+
+
+function drawRuneCirlce(futhark, centerX, centerY, r){
+    let angleStep = 360/futhark.length;
+    
+    for (let i = 0; i < futhark.length; i++) {
+        const rune = futhark[i];
+        let angle = angleStep * i - 90;
+        let x = centerX + r * cos(angle);
+        let y = centerY + r * sin(angle);
+        push()
+        translate(x, y)
+        rotate(angle+90)
+        drawRune(rune, 0, 0, "magenta", 3, 0, 0.5);
+        pop()
     }
 }
--- tests/sketch.js
+++ tests/sketch.js
@@ -12,26 +12,5 @@ function draw() {
     //let stroke = map(sin(frameCount/20), -1, 1, 2, 5)
     //drawRune(isa, 300, 100, "blue", 3)
     //drawRune(ansuz, 400, 100, "blue", 3)
-    drawFootark(futhark, 50, 50, "red", 2)
-}
-
-
-function drawFootark(futhark, x, y, _color, _stroke) {
-    let startX = x
-    let startY = y
-    let stepW = 100
-    let stepH = 200
-
-    for (let i = 0; i < futhark.length; i++) {
-        const rune = futhark[i];
-        drawRune(rune, startX, startY, _color, _stroke);
-        startX += stepW
-        
-        if ((i + 1)  % 8 == 0) {
-            startY += stepH
-            startX = 50
-        }
-        
-    }
-
+    drawFuthark(futhark, 50, 50, "red", 2)
 }
